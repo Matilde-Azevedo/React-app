b379c6e3d02d677a1f51b519f0ac654c129f0bbc
Matilde Azevedo <matildeazevedot19@gmail.com>
2026-04-10 00:18:35 +0100

    Mandando para o github

--- src/services/axios.jsx
+++ src/services/axios.jsx
@@ -1,9 +1,7 @@
-//http://192.168.1.13
-
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://13.63.70.49',
+  baseURL: import.meta.env.VITE_API_URL,
 })
 
 export default api
